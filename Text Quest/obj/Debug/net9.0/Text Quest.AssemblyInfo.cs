e510b01487720ee15d59d8f6b47c0c804f4876f2
koroznikita@gmail.com <koroznikita@gmail.com>
2025-09-25 09:25:58 +0300

    begin

--- Text Quest/obj/Debug/net9.0/Text Quest.AssemblyInfo.cs
+++ Text Quest/obj/Debug/net9.0/Text Quest.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Text Quest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a74fdf5381de416c957269a9343b6e0d4c9a8c9")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Text Quest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Text Quest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
